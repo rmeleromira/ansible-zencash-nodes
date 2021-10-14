22711da3e87188442ecbd9b0233d39df17eab673
DC <dc@technoexile.com>
2021-10-14 14:14:25 -0600

    use python3-lxc

--- install_host_deps.sh
+++ install_host_deps.sh
@@ -2,7 +2,7 @@
 set -x
 
 sudo apt update
-sudo apt install python3 python3-pip python-lxc libssl-dev git -y
+sudo apt install python3 python3-pip python3-lxc libssl-dev git -y
 pip3 install --upgrade setuptools
 pip3 install ansible
 pip3 install -U cryptography
