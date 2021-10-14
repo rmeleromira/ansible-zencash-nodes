55fbfbaeba3699e9654c96eab77acfb9c2cfdfbe
DC <dc@technoexile.com>
2021-10-14 15:07:58 -0600

    cp

--- install_host_deps.sh
+++ install_host_deps.sh
@@ -6,3 +6,6 @@ sudo apt install python3 python3-pip python3-lxc libssl-dev git -y
 pip3 install --upgrade setuptools
 pip3 install ansible
 pip3 install -U cryptography
+
+echo 'installed ansible to ~/.local/bin, make sure this is in $PATH'
+
